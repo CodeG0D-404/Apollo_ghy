7c4d6f5a8e68d9125d40097b2f0b48874ec1581b
CodeG0D-404 <code010001110100111101000100@gmail.com>
2026-02-25 12:27:23 +0530

    edit doctor hard url fixed

--- apollo/src/clinic/EditDoctor.jsx
+++ apollo/src/clinic/EditDoctor.jsx
@@ -22,7 +22,7 @@ const LANGUAGE_OPTIONS = [
   "Malayalam","Odia","Punjabi",
 ];
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const EditDoctor = () => {
   const { id } = useParams();
